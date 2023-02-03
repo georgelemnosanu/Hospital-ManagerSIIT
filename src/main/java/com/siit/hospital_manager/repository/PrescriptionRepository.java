2b504f1414900ff7aa685623fad6f667bd90c748
georgelemnosanu <georgelemnosanu@gmail.com>
2023-02-03 21:10:16 +0200

    removed prescription

--- src/main/java/com/siit/hospital_manager/repository/PrescriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.siit.hospital_manager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PrescriptionRepository extends JpaRepository<Prescription, Integer> {
-
-}
--- src/main/java/com/siit/hospital_manager/service/PrescriptionService.java
+++ src/main/java/com/siit/hospital_manager/service/PrescriptionService.java
@@ -1,7 +1,5 @@
 package com.siit.hospital_manager.service;
 
-import com.siit.hospital_manager.repository.MedsRepository;
-import com.siit.hospital_manager.repository.PrescriptionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
