9e309ee831240c92f3371eb54283c4282063f9f8
georgelemnosanu <georgelemnosanu@gmail.com>
2023-02-06 18:43:37 +0200

    Create doctor

--- src/main/java/com/siit/hospital_manager/repository/DoctorRepository.java
+++ src/main/java/com/siit/hospital_manager/repository/DoctorRepository.java
@@ -3,6 +3,9 @@ package com.siit.hospital_manager.repository;
 import com.siit.hospital_manager.model.Doctor;
 import com.siit.hospital_manager.model.Specialty;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +17,5 @@ public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
     Optional<Doctor> findByName(String name);
 
     List<Doctor> findAllBySpecialty(Specialty specialty);
+
 }
